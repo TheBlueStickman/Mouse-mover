ef15e51a28774bbdaac227cb4179915a6dd7663f
TheBlueStickman <148999994+TheBlueStickman@users.noreply.github.com>
2026-08-18 14:38:18 +0200

    Created function for checking ESC while counting to 1s

--- main.py
+++ main.py
@@ -2,45 +2,66 @@ import pyautogui, time, keyboard
 
 time.sleep(1)
 
-left corner Point(x=734, y=354)
-right corner Point(x=959, y=350)
-
+# left corner Point(x=734, y=354)
+# right corner Point(x=959, y=350)
 #right down Point(x=960, y=579)
 #left down Point(x=731, y=582)
 
 programRun = True
 
 def checkEsc():
-exit = False
-if keyboard.is_pressed('esc'):
-	print("ESC pressed. Breaking the loop.")
-	exit = True
-return exit
+    exit = False
+    if keyboard.is_pressed('esc'):
+        print("ESC pressed. Breaking the loop.")
+        exit = True
+    return exit
+
+def couunt():
+    while countdown >= seconds:
+        if checkEsc() == True:
+            break
+        else:
+            time.sleep(0.01)
+            countdown += 0.01
+        programRun = False
+
+seconds = 1
+
+countdown = 0
 
 while (programRun):
-	if checkEsc() == True:
-		programRun = False
+    if checkEsc() == True:
+        programRun = False
 
-	pyautogui.moveTo(730, 350)
-	time.sleep(1)
+    pyautogui.moveTo(750, 350)
 
-	if checkEsc() == True:
-    		break    
+    while countdown >= seconds:
+        if checkEsc() == True:
+            break
+        else:
+            time.sleep(0.01)
+            countdown += 0.01
+        programRun = False
 
-	pyautogui.moveTo(960, 350)
-	time.sleep(1)
+    if checkEsc() == True:
+        programRun = False
 
-	if checkEsc() == True:
-    		programRun = False
+    pyautogui.moveTo(950, 350)
+    time.sleep(seconds)
 
-	pyautogui.moveTo(960, 580)
-	time.sleep(1)
+    if checkEsc() == True:
+        programRun = False
 
-	if checkEsc() == True:
-    		programRun = False
+    pyautogui.moveTo(950, 550)
+    time.sleep(seconds)
 
-	pyautogui.moveTo(730, 580)
-	time.sleep(1)
+    if checkEsc() == True:
+        programRun = False
 
-	if checkEsc() == True:
-    		programRun = False
+    pyautogui.moveTo(750, 550)
+    time.sleep(seconds)
+    
+    if checkEsc() == True:
+        programRun = False
+
+somethign like this?
