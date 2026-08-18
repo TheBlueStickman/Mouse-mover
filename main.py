967520b53a30d74291f82e42edc07ab75a3654e2
TheBlueStickman <148999994+TheBlueStickman@users.noreply.github.com>
2026-08-18 14:54:39 +0200

    Cleaned up code and added comments

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import pyautogui, time, keyboard
 
 time.sleep(1)
 
-# left corner Point(x=734, y=354)
-# right corner Point(x=959, y=350)
-#right down Point(x=960, y=579)
-#left down Point(x=731, y=582)
-
 seconds = 1
 
+# function for checking if ESC is being pressed, if it is pressed it returns True else it is False
 def checkEsc():
     exit = False
     if keyboard.is_pressed('esc'):
@@ -16,6 +12,8 @@ def checkEsc():
         exit = True
     return exit
 
+# function that while counting to the amount of seconds (not fully accurate amount of time) also keeps checking if ESC is being pressed or no
+# according to that it returns True or False
 def waitAndCheck():
     exit = False
     countdown = 0
@@ -30,18 +28,11 @@ def waitAndCheck():
 
 
 while (True):
+    # If the waitAndCheck function returns True, ESC was pressed, so it will break out of the main loop, ending the program.
+    # Else move to the specified point
     if waitAndCheck() == True:
         break
     pyautogui.moveTo(750, 350)
-
-#    while countdown >= seconds:
-#        countdown = 0
- #       if checkEsc() == True:
- #           break
- #       else:
-   #         time.sleep(0.01)
-  #          countdown += 0.01
-   #     programRun = False
 
     if waitAndCheck() == True:
         break
@@ -54,5 +45,3 @@ while (True):
     if waitAndCheck() == True:
         break
     pyautogui.moveTo(750, 550)
-
-this seems to working as I would want it to
