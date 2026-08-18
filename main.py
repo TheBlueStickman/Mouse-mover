4df01fe5a4acca4619c6b15517f8632ab11e9dd0
TheBlueStickman <148999994+TheBlueStickman@users.noreply.github.com>
2026-08-18 14:28:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pyautogui, time, keyboard, threading
+
+time.sleep(1)
+
+left corner Point(x=734, y=354)
+right corner Point(x=959, y=350)
+
+#right down Point(x=960, y=579)
+#left down Point(x=731, y=582)
+
+programRun = True
+
+while (programRun):
+
+if keyboard.is_pressed('esc'):
+    print("ESC pressed. Breaking the loop.")
+    programRun = False
+
+if programRun == False:
+    break
+
+
+pyautogui.moveTo(730, 350)
+time.sleep(5)
+
+pyautogui.moveTo(960, 350)
+time.sleep(5)
+
+pyautogui.moveTo(960, 580)
+time.sleep(5)
+
+pyautogui.moveTo(730, 580)
+time.sleep(5)
